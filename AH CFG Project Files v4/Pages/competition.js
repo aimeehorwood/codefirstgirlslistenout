c20b5991db43e44dc84f446f2a3d65df267d3855
axh298 <107215233+axh298@users.noreply.github.com>
2022-08-11 20:02:16 +0100

    Add files via upload

---
+++ AH CFG Project Files v4/Pages/competition.js
@@ -0,0 +1,14 @@
+
+
+var firstName=prompt("What is your name?");
+alert("Hello "  +  firstName);
+
+var age=prompt("How old are you? ");
+
+
+if (age <= 18) {
+    alert ('Sorry come back when you are 18!');
+}
+else {
+    alert ('Awesome! You are over 18!');
+}
